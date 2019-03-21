70b2a105ba01408ae52c94dfe6468f9d740e2aa7
emrekepenek <emrekepenek38@gmail.com>
2019-03-22 02:22:58 +0300

    solved some sever problem.

--- src/main/java/koala/sloth/demo/bootstrap/bootStrapData.java
+++ src/main/java/koala/sloth/demo/bootstrap/bootStrapData.java
@@ -21,45 +21,63 @@ public class bootStrapData implements CommandLineRunner {
     public void run(String... args) throws Exception {
        System.out.println("Loading customer data");
 
-        /*Customer c1 = new Customer();
-        c1.setNAME("yogurt");
-        c1.setBRAND("sutas");
-        c1.setCATEGORY("Hayvansal");
+        Customer c1 = new Customer();
+        c1.setNAME("Apple");
+        c1.setBRAND("");
+        c1.setCATEGORY("Fruit");
         c1.setInTheFridge(0);
-        c1.setPRICE(5);
+        c1.setPRICE((float) 2.00);
         c1.setPRICE_UNIT("TL");
         c1.setPHYSICAL_UNIT("KG");
-        c1.setFIRST_DATE((long) 222222222);
+        c1.setFIRST_DATE((long) 1551615240120L);
         customerRepository.save(c1);
 
         Customer c2 = new Customer();
-        c2.setNAME("kola");
-        c2.setBRAND("pepsi");
-        c2.setCATEGORY("icecek");
+        c2.setNAME("Tomato");
+        c2.setBRAND("");
+        c2.setCATEGORY("Vegetable");
         c2.setInTheFridge(0);
-        c2.setPRICE(3);
+        c2.setPRICE((float) 2.00);
         c2.setPRICE_UNIT("TL");
-        c2.setPHYSICAL_UNIT("Birim");
-        c2.setFIRST_DATE((long) 3333333);
+        c2.setPHYSICAL_UNIT("KG");
+        c2.setFIRST_DATE((long) 1551615240120L);
         customerRepository.save(c2);
 
         Customer c3 = new Customer();
-        c3.setNAME("bal");
-        c3.setBRAND("balparmak");
-        c3.setCATEGORY("Hayvansal");
-        c3.setInTheFridge(1);
-        c3.setPRICE(20);
+        c3.setNAME("Terderloin");
+        c3.setBRAND("");
+        c3.setCATEGORY("Meat");
+        c3.setInTheFridge(0);
+        c3.setPRICE((float) 10.00);
         c3.setPRICE_UNIT("TL");
         c3.setPHYSICAL_UNIT("KG");
-        c3.setFIRST_DATE((long) 4444444);
+        c3.setFIRST_DATE((long) 1551615240120L);
         customerRepository.save(c3);
+
+        Customer c4 = new Customer();
+        c4.setNAME("Fanta");
+        c4.setBRAND("CocaCola");
+        c4.setCATEGORY("Drink");
+        c4.setInTheFridge(0);
+        c4.setPRICE((float) 3.25);
+        c4.setPRICE_UNIT("TL");
+        c4.setPHYSICAL_UNIT("piece");
+        c4.setFIRST_DATE((long) 1551615240120L);
+        customerRepository.save(c4);
+
+        Customer c5 = new Customer();
+        c5.setNAME("Pepsi Cola");
+        c5.setBRAND("Pepsi");
+        c5.setCATEGORY("Drink");
+        c5.setInTheFridge(0);
+        c5.setPRICE((float) 3.00);
+        c5.setPRICE_UNIT("TL");
+        c5.setPHYSICAL_UNIT("piece");
+        c5.setFIRST_DATE((long) 1551615240120L);
+        customerRepository.save(c5);
+
 
         System.out.println("Customer Saved : "+customerRepository.count());
 
-        Order o1 = new Order();
-        o1.setPRODUCT_ID(1);
-        o1.setQUANTITY(3);
-        o1.setDATE((long) 333333);
-        orderRepository.save(o1);*/
     }
 }
